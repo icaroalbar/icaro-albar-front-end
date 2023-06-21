ffa2cec34c1b24abb7cfdffc184c9afccb9d0f3e
Icaro Albar <icaro.albar@live.com>
2023-06-20 21:07:55 -0300

    :lipstick: feat: carousel adjustment

--- src/components/Stacks/index.tsx
+++ src/components/Stacks/index.tsx
@@ -6,8 +6,22 @@ import { responsive } from "./responsive";
 
 export function Stacks(): ReactElement {
   return (
-    <div className="p-10">
-      <AliceCarousel mouseTracking items={items} responsive={responsive} />
+    <div className="py-16 select-none text-white text-center">
+      <h6 className="text-h6 font-normal mb-6">Skills</h6>
+      <p className="pb-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sunt
+        nostrum hic ad ex quia commodi nemo repudiandae!
+      </p>
+      <AliceCarousel
+        mouseTracking
+        items={items}
+        infinite={true}
+        autoPlay
+        disableButtonsControls
+        disableDotsControls
+        animationDuration={2000}
+        responsive={responsive}
+      />
     </div>
   );
 }
