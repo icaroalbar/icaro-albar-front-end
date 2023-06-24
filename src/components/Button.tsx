a27e773d680693fb0b9d5191261d142b71fcd8a5
Icaro Albar <icaro.albar@live.com>
2023-06-24 17:34:21 -0300

    :bug: fix: adjusting the button's text color bug

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -29,7 +29,7 @@ export const Button = ({
         className={clsx(
           `uppercase font-medium select-none rounded transition-colors border-2 shadow-md outline-none bg-transparent border-primary-10 hover:bg-blue-900 disabled:bg-blue-900 focus:shadow-blue-500/50 ${
             disabled && "bg-opacity-40 pointer-events-none"
-          }`,
+          } ${title && "text-white"}`,
           {
             "px-6 py-2 text-sm": size == "sm",
             "px-10 py-3 text-base": size == "md",
